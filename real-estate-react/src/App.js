68e1bd9296092275915382e37f7f20a31432bc0e
neyneyalldayday <drfrankenstein123@gmail.com>
2021-01-14 16:37:07 -0600

    realestate site first commit

---
+++ real-estate-react/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import Navbar from "../src/components/Navbar"
+import Hero from './components/Hero';
+import GlobalStyle from './globalStyles';
+import { SliderData } from './data/SliderData';
+
+
+function App() {
+  return (
+    <>
+    <GlobalStyle />
+    <Navbar /> 
+    <Hero slides={SliderData}/>  
+   </>
+  );
+}
+
+export default App;
---
+++ real-estate-react/src/data/SliderData.js
@@ -0,0 +1,40 @@
+import ImageOne from '../images/house-1.jpg'
+import ImageTwo from '../images/house-2.jpg'
+import ImageThree from '../images/house-3.jpg'
+import ImageFour from '../images/house-4.jpg'
+
+export const SliderData = [
+
+    {
+        title: "shitty houses",
+        price: "16 toyota priuses",
+        path: '/homes',
+        label: 'View Home',
+        image: ImageOne,
+        alt: 'House'
+    },
+    {
+        title: "shitty houses",
+        price: "20 hot dogs",
+        path: '/homes',
+        label: 'View Home',
+        image: ImageTwo,
+        alt: 'House'
+    },
+    {
+        title: "shitty houses",
+        price: "16 Robux",
+        path: '/homes',
+        label: 'View Home',
+        image: ImageThree,
+        alt: 'House'
+    },
+    {
+        title: "shitty houses",
+        price: "40,000,000,000,000 pennies",
+        path: '/homes',
+        label: 'View Home',
+        image: ImageFour,
+        alt: 'House'
+    },
+];
